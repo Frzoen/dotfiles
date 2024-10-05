46e5f9583f2f948e2ee5d29d0f04cfa427ba5541
Sebastian Mróz <mrozix11@gmail.com>
2024-10-06 00:03:25 +0200

    Nvim: format neo-tree.lua

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -7,6 +7,6 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set('n', '<leader>e',':Neotree filesystem focus left<CR>',{})
-  end
+    vim.keymap.set("n", "<leader>e", ":Neotree filesystem focus left<CR>", {})
+  end,
 }
