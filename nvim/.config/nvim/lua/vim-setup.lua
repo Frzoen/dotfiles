84fe3b598d6db3d34ece55030a3d3af2af3e9d61
Sebastian Mróz <mrozix11@gmail.com>
2024-11-03 01:40:07 +0100

    Nvim: add toggling relative numbers

--- nvim/.config/nvim/lua/vim-setup.lua
+++ nvim/.config/nvim/lua/vim-setup.lua
@@ -13,6 +13,15 @@ vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
+-- toggle relative number
+
+vim.keymap.set("n", "<leader>rn", ":set relativenumber! <CR>")
+
+-- escape to clear highlight
+vim.keymap.set("n", "<Esc>", function()
+  vim.cmd("noh")
+end, {})
+
 -- center search line
 vim.keymap.set("c", "<CR>", function()
   return vim.fn.getcmdtype() == "/" and "<CR>zzzv" or "<CR>"
