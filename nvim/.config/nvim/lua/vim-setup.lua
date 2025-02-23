4de71a9d1a15e2ee977af230374ca2f3a6b4fea4
Sebastian Mroz <smroz@antmicro.com>
2025-02-23 22:45:30 +0100

    More vim remaps

--- nvim/.config/nvim/lua/vim-setup.lua
+++ nvim/.config/nvim/lua/vim-setup.lua
@@ -7,6 +7,18 @@ vim.g.background = "light"
 
 vim.opt.swapfile = false
 
+-- Sane escape
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", {noremap = false})
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Set highlight on search
+vim.o.hlsearch = true
+
 -- navigate vim panes better
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
@@ -21,6 +33,16 @@ vim.keymap.set("n", "<leader>rn", ":set relativenumber! <CR>")
 vim.keymap.set("n", "<Esc>", function()
   vim.cmd("noh")
 end, {})
+
+-- center the search
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- copy to system clipboard
+vim.opt.clipboard = 'unnamedplus'
+
+-- twilight
+vim.api.nvim_set_keymap("n", "tw", ":Twilight<enter>", {noremap = false})
 
 -- center search line
 vim.keymap.set("c", "<CR>", function()
