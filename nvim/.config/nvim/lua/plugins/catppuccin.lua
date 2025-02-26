5b12ce7292fd6ca3c91558ffe63c1270b74a6463
Sebastian Mroz <smroz@antmicro.com>
2025-02-26 22:45:41 +0100

    Integrate catppuccin with more things

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -6,6 +6,17 @@ return {
   config = function()
     require("catppuccin").setup({
       transparent_background = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        neotree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+            enabled = true,
+            indentscope_color = "",
+        },
+    }
     })
     vim.cmd.colorscheme("catppuccin")
   end,
